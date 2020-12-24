561cb3d2f4ed528cd36b1a2812a9bde3d6ae2b64
Basem Prince <basemprince@Basems-MacBook-Pro-2.local>
2020-12-24 01:38:45 +0100

    fix main page

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,9 +1,11 @@
 //* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
 var dropdown = document.getElementsByClassName("dropdown-btn");
+
 var i;
 var j;
 for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function() {
+    
     this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
     if (dropdownContent.style.display === "block") {
@@ -12,7 +14,7 @@ for (i = 0; i < dropdown.length; i++) {
       
       for (j = 0; j < dropdown.length; j++) {
         if (i!== j){
-            dropdown[j].nextElementSibling.style.display = "none"
+            //  dropdown[j].nextElementSibling.style.display = "none"
         }
         
       }
