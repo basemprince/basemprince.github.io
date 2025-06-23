526864d76e493a2127a8b640537923ffad8d04fa
Sam Prince <sam@Sams-MacBook-Pro.local>
2025-06-23 18:16:52 -0500

    fixed the endpoint address

--- assets/js/chatbot.js
+++ assets/js/chatbot.js
@@ -50,7 +50,7 @@ async function handleSend() {
   }, 400);
 
   try {
-    const res = await fetch("http://127.0.0.1:8000/chat", {
+    const res = await fetch("https://basem-chatbot.onrender.com/chat", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text: userText })
